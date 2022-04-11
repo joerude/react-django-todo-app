17114e95846c70006d418e9ee353711c8cd0f513
ma4akk <ma4akk@gmail.com>
2022-04-11 18:10:08 +0300

    refactoring codestyle, deleting unnecessary files

--- api/admin.py
+++ api/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 
+from .models import Task
+
 # Register your models here.
 
-from .models import Task
 admin.site.register(Task)
--- api/views.py
+++ api/views.py
@@ -1,11 +1,11 @@
-from django.shortcuts import render
 from django.http import JsonResponse
-
+from django.shortcuts import render
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
-from .serializers import TaskSerializer
 
 from .models import Task
+from .serializers import TaskSerializer
+
 # Create your views here.
 
 @api_view(['GET'])
